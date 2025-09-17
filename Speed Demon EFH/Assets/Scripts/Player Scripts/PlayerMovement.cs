aa6663431191060a214e4af2b9637cd1ea9bed2d
MUKILTEO\1725219 <george.chmykhalo@gmail.com>
2025-09-17 09:56:21 -0700

    Movement done (no coyote time yet)

--- Speed Demon EFH/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Speed Demon EFH/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -9,10 +9,15 @@ public class PlayerMovement : MonoBehaviour
     public Transform groundCheck;
     public float groundRadius = 0.2f;
     public LayerMask groundLayer;
+    
 
     private Rigidbody2D rb;
     private float accelerationTimerX = 0f;
     private bool isGrounded = false;
+    private float airResist = 1f;
+
+    //coyote time variables
+    private float coyoteCounter = 4f;
 
     void Start()
     {
@@ -25,25 +30,34 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         // Exponential acceleration for X
-        if (moveX != 0){
+        if (moveX != 0) {
             accelerationTimerX += Time.deltaTime;
-        }else{
+        } else {
             accelerationTimerX = 0f;
         }
-        // Exponential acceleration for Y
+        if (!isGrounded) {
+            coyoteCounter -= 1f;
+        }else {
+            coyoteCounter = 40f;
+        }
+        // Exponential acceleration for 
 
         float velocityX = maxSpeed * moveX * (1 - Mathf.Exp(-accelerationRate * accelerationTimerX));
         float velocityY = rb.linearVelocity.y;
-
-        rb.linearVelocity = new Vector2(velocityX, velocityY);
+        if (isGrounded) {
+            airResist = 1f;
+        }
+        else {
+            airResist = 0.8f;
+        }
+        rb.linearVelocity = new Vector2(velocityX*airResist, velocityY);
         // Ground check
-        if (groundCheck != null){
+        if (groundCheck != null) {
             isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
         }
         // Jump input (spacebar)
-        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded){
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && coyoteCounter >= 0){
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-
         }
     }
 }
