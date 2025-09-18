e9a7c5a8ea624dee588991d7b16988a77957c1c8
MUKILTEO\1725375 <azenosnothere@gmail.com>
2025-09-18 10:18:43 -0700

    smoothing camera movement (wip)

---
+++ Speed Demon EFH/Assets/CameraMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // initialize variables
+    public GameObject player;
+    private Vector3 cameraOffset;
+
+    private float cameraSmoothing = 20;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraOffset.x = player.transform.position.x - transform.position.x;
+        cameraOffset.y = player.transform.position.y - transform.position.y;
+
+        transform.position = transform.position + cameraOffset/cameraSmoothing;
+
+        print(transform.position);
+        print(player.transform.position);
+    }
+}
